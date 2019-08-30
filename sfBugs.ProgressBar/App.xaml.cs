551fc77617140d0f9ec70d2406547617da981770
Davide Dolla <davide.dolla@outlook.it>
2019-08-30 19:13:53 +0200

    1st commit

---
+++ sfBugs.ProgressBar/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace sfBugs.ProgressBar
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new NavigationPage( new MainPage());		//DD 30/08/2019 15:46:53	deve esse in un navPage
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ sfBugs.ProgressBar/Auth_RegisterPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace sfBugs.ProgressBar
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Auth_RegisterPage : ContentPage
+	{
+		public Auth_RegisterPage()
+		{
+			InitializeComponent();
+			NavigationPage.SetHasNavigationBar(this, false);
+
+			btnCancel.Clicked += async (s, e) =>
+			{
+				await Navigation.PopAsync(false);
+			};
+		}
+	
+		public int StepActive = 1;
+		public int StepMax = 3;
+		private void BtnStepBack_Clicked(object sender, EventArgs e)
+		{
+			if(StepActive>1) StepActive--;
+			ShowStepActive();
+		}
+		private void BtnStepForward_Clicked(object sender, EventArgs e)
+		{
+			if (StepActive < StepMax) StepActive++;
+			ShowStepActive();
+		}
+
+		private void ShowStepActive()
+		{
+			for (int i = 1; i <= StepMax; i++)
+			{
+				var stackLayoutArea = this.FindByName($"step{i}") as StackLayout;
+				var stepView = stepProgressBar.Children[i - 1] as Syncfusion.XForms.ProgressBar.StepView;
+
+				if (i == StepActive)
+				{
+					stackLayoutArea.IsVisible = true;
+					stepView.Status = Syncfusion.XForms.ProgressBar.StepStatus.InProgress;
+				}
+				else
+				{
+					stackLayoutArea.IsVisible = false;
+					if (i > StepActive)
+						stepView.Status = Syncfusion.XForms.ProgressBar.StepStatus.NotStarted;
+					else
+						stepView.Status = Syncfusion.XForms.ProgressBar.StepStatus.Completed;
+				}
+				stepView.SecondaryText = stepView.Status.ToString();
+			}
+		}
+	}
+}
