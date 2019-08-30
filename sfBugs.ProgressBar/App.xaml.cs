69f6375ecad39595ddc8fd35532b4a12afe98fbe
Davide Dolla <davide.dolla@outlook.it>
2019-08-30 23:56:05 +0200

    Added option to call ShowStepActive in async way OnMainThread ... but the problem remain

--- sfBugs.ProgressBar/App.xaml.cs
+++ sfBugs.ProgressBar/App.xaml.cs
@@ -9,23 +9,7 @@ namespace sfBugs.ProgressBar
 		public App()
 		{
 			InitializeComponent();
-
-			MainPage = new NavigationPage( new MainPage());		//DD 30/08/2019 15:46:53	deve esse in un navPage
-		}
-
-		protected override void OnStart()
-		{
-			// Handle when your app starts
-		}
-
-		protected override void OnSleep()
-		{
-			// Handle when your app sleeps
-		}
-
-		protected override void OnResume()
-		{
-			// Handle when your app resumes
+			MainPage = new NavigationPage( new MainPage());
 		}
 	}
 }
--- sfBugs.ProgressBar/Auth_RegisterPage.xaml.cs
+++ sfBugs.ProgressBar/Auth_RegisterPage.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -20,19 +16,35 @@ namespace sfBugs.ProgressBar
 			{
 				await Navigation.PopAsync(false);
 			};
+			this.BindingContext = this;
 		}
-	
+		public bool _UseAsync = false;
+
+		public bool UseAsync { get => _UseAsync; set { if (value != _UseAsync) { _UseAsync = value; OnPropertyChanged(); } } }
+
 		public int StepActive = 1;
 		public int StepMax = 3;
 		private void BtnStepBack_Clicked(object sender, EventArgs e)
 		{
 			if(StepActive>1) StepActive--;
-			ShowStepActive();
+			if (UseAsync)
+				ShowStepActiveAsync();
+			else
+				ShowStepActive();
 		}
+
 		private void BtnStepForward_Clicked(object sender, EventArgs e)
 		{
 			if (StepActive < StepMax) StepActive++;
-			ShowStepActive();
+			if (UseAsync)
+				ShowStepActiveAsync();
+			else
+				ShowStepActive();
+		}
+
+		private void ShowStepActiveAsync()
+		{
+			Device.BeginInvokeOnMainThread(new Action(ShowStepActive));
 		}
 
 		private void ShowStepActive()
